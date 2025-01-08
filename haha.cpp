92c60c46eb06884b94bd28c6af82294f83b899b8
VIDHITTS <vidhitts@gmail.com>
2025-01-08 18:50:31 +0530

    1st lec

--- haha.cpp
+++ haha.cpp
@@ -1,1 +1,31 @@
+// #include<iostream>
+// using namespace std;
 
+// int main(){
+//     return 0;
+// }
+//boilerplate
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"hello world\n hahah";
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age=25;
+    char grade='a';
+    float PI=3.13f; 
+    double ha=89.99999;
+    bool issafe=true;
+    int value=grade;
+    int val=(int)ha;
+    cout<< val<<endl; //sizeof of used for knowing the size of an variable
+    return 0;
+}
