1a8313357613c1f80d750a04ea9be298ba42b6d8
VIDHITTS <vidhitts@gmail.com>
2025-01-09 23:03:28 +0530

    kjbv s

--- haha.cpp
+++ haha.cpp
@@ -32,3 +32,20 @@ int main(){
     cout<< val<<endl; //sizeof of used for knowing the size of an variable
     return 0;
 }
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age=25;
+    char grade='a';
+    float PI=3.13f; 
+    double ha=89.99999;
+    bool issafe=true;
+    int value=grade;
+    int val=(int)ha;
+    int a;
+    cin>>a;
+    cout<<a;
+    cout<< val<<endl; //sizeof of used for knowing the size of an variable
+    return 0;
+}
